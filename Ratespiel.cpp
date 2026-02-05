b4faac721be3d72cacda70c561b4e2305169908e
Klaus2053 <klausrochol@hotmail.com>
2026-02-05 12:16:11 +0100

    Add files via upload

---
+++ Ratespiel.cpp
@@ -0,0 +1,46 @@
+//Erstellt von Klaus Barth Rochol
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int Host, Player;
+
+    // Spieler 1 gibt eine Zahl ein
+    cout << "Spieler 1: Denke dir eine Zahl: ";
+    cin >> Host;
+
+    if (Host < 1 or Host > 100)
+
+    {
+        cout << "Bitte gib eine Zahl ein!" << endl;
+        return 1; // Programm mit Fehlercode beenden
+    }
+
+    /*do while ist anscheinend ein wiederhol bis es richtig ist mit den if kommand oder so*/
+do {
+        cout << "Spieler 2: Rate die Zahl: ";
+        cin >> Player;
+
+        if (Player < Host)
+        {
+        cout << "Zu klein!" << endl;
+        }
+
+
+        else if (Player > Host)
+        {
+        cout << "Zu groß!" << endl;
+        }
+
+
+        else
+        {
+        cout << "Richtig geraten!" << endl;
+        }
+
+}
+    while (Player != Host);
+
+    return 0;
+}
